cfb57e294c24aee476091eb64b889fd215bb2a28
MD SAIF ULLAH <mdsaifullah.wd@gmail.com>
2022-03-02 20:09:34 +0600

    add error message

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
+const toggleElementDisplay = (id, displayStyle) => {
+  document.getElementById(id).style.display = displayStyle;
+}
 const displayPhones = () => {
   const searchText = document.getElementById('search-field').value;
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   fetch(url)
   .then(res => res.json())
   .then(data => {
+    console.log(data)
+    const detailResult = document.getElementById('detail-result');
+    detailResult.textContent = '';
     const containerDiv = document.getElementById('container-div');
     containerDiv.textContent = ''
-
-  const detailResult = document.getElementById('detail-result');
-  detailResult.textContent = ''
-    for (const phone of data.data) {
-      // console.log(phone)
-      const div = document.createElement('div');
-      div.innerHTML = `
-      <div class="card" style="width: 18rem">
-        <img src=${phone.image} class="card-img-top" alt="..." />
-        <div class="card-body">
-        <h5 class="card-title">${phone.phone_name}</h5>
-        <p class="card-text">
-        ${phone.brand}
-        </p>
-        </div>
-        <div class="card-body">
-        <button id=${phone.slug} onclick="displayDetails(this.id)" class="btn btn-dark">Details</button>
-        </div>
-        </div>
-        `
-        div.classList.add('col-md-4')
-        containerDiv.appendChild(div)
-        document.getElementById('search-field').value = ''
+    if(!data.data.length) {
+      toggleElementDisplay('error-message','block')
+    }
+    else {
+      toggleElementDisplay('error-message','none')
+      for (const phone of data.data) {
+        // console.log(phone)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card" style="width: 18rem">
+          <img src=${phone.image} class="card-img-top" alt="..." />
+          <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">
+          ${phone.brand}
+          </p>
+          </div>
+          <div class="card-body">
+          <button id=${phone.slug} onclick="displayDetails(this.id)" class="btn btn-dark">Details</button>
+          </div>
+          </div>
+          `
+          div.classList.add('col-md-4')
+          containerDiv.appendChild(div)
+          document.getElementById('search-field').value = ''
+      }
     }
   })
-}
-
+  }
 const displayDetails = (clicked_id) => {
   const detailResult = document.getElementById('detail-result');
   detailResult.textContent = ''
