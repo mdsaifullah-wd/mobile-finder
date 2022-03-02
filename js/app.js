e25503245a45e5c0ee3984f971f908d83433efcc
MD SAIF ULLAH <mdsaifullah.wd@gmail.com>
2022-03-02 19:14:55 +0600

    add product details dynamically

--- js/app.js
+++ js/app.js
@@ -6,8 +6,11 @@ const displayPhones = () => {
   .then(data => {
     const containerDiv = document.getElementById('container-div');
     containerDiv.textContent = ''
+
+  const detailResult = document.getElementById('detail-result');
+  detailResult.textContent = ''
     for (const phone of data.data) {
-      console.log(phone)
+      // console.log(phone)
       const div = document.createElement('div');
       div.innerHTML = `
       <div class="card" style="width: 18rem">
@@ -19,7 +22,7 @@ const displayPhones = () => {
         </p>
         </div>
         <div class="card-body">
-        <button id=${phone.slug} onclick="displayDetails()" class="btn btn-dark">Details</button>
+        <button id=${phone.slug} onclick="displayDetails(this.id)" class="btn btn-dark">Details</button>
         </div>
         </div>
         `
@@ -30,7 +33,52 @@ const displayPhones = () => {
   })
 }
 
-const displayDetails = () => {
+const displayDetails = (clicked_id) => {
   const detailResult = document.getElementById('detail-result');
   detailResult.textContent = ''
+  const url = `https://openapi.programming-hero.com/api/phone/${clicked_id}`
+  // console.log(url)
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    const release = data.data.releaseDate ? data.data.releaseDate: "No Release Date Found"
+    const div = document.createElement('div');
+    
+  div.innerHTML = `
+    <div class="col-md-3">
+      <img class="w-100" src=${data.data.image}>
+    </div>
+    <div class="col-md-9 my-auto">
+      <h2>${data.data.name}</h2>
+      <p>${release}</p>
+      <h3>Main Features</h3>
+      <ul>
+        <li>Storage: ${data.data.mainFeatures.storage}</li>
+        <li>Display Size: ${data.data.mainFeatures.displaySize}</li>
+        <li>Chipset: ${data.data.mainFeatures.chipSet}</li>
+        <li>Memory: ${data.data.mainFeatures.memory}</li>
+        <li>Sensors: ${data.data.mainFeatures.sensors}</li>
+      </ul>
+    </div>
+  `
+  div.classList.add('row')
+detailResult.appendChild(div)
+})  
 }
+
+
+
+
+
+
+
+/* data: {mainFeatures: {storage: "128GB/256GB storage, microSDXC",…}, slug: "samsung_galaxy_tab_s8+-11342",…}
+brand: "Samsung"
+image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-s8-plus.jpg"
+mainFeatures: {storage: "128GB/256GB storage, microSDXC",…}
+name: "Galaxy Tab S8+"
+others: {WLAN: "Wi-Fi 802.11 a/b/g/n/ac/6e, dual-band, Wi-Fi Direct, hotspot", Bluetooth: "5.2, A2DP, LE",…}
+releaseDate: "Exp. release 2022, February"
+slug: "samsung_galaxy_tab_s8+-11342"
+status: true */
