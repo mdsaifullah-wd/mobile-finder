50b36c3147493ee0652e3d568e85b33c584fedac
MD SAIF ULLAH <mdsaifullah.wd@gmail.com>
2022-03-02 23:08:21 +0600

    clean code

--- js/app.js
+++ js/app.js
@@ -1,28 +1,45 @@
+// Tootle Element Display Style
 const toggleElementDisplay = (id, displayStyle) => {
   document.getElementById(id).style.display = displayStyle;
-}
-const displayPhones = () => {
-  const searchText = document.getElementById('search-field').value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+};
+
+// Display Search Result
+const displaySearchResults = () => {
+  // Get Search Text
+  const searchText = document
+    .getElementById("search-field")
+    .value.toLowerCase();
+  // Make Dynamic API url
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  // Fetch API
   fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    const detailResult = document.getElementById('detail-result');
-    detailResult.textContent = '';
-    const containerDiv = document.getElementById('container-div');
-    containerDiv.textContent = ''
-    if(!data.data.length) {
-      toggleElementDisplay('error-message','block')
-    }
-    else {
-      toggleElementDisplay('error-message','none')
-      const phones = Object.keys(data.data).slice(0,20).map(key => ({0:data.data[key]}));
-      console.log(phones)
-      for (const phone of phones) {
-        console.log(phone)
-        const div = document.createElement('div');
-        div.innerHTML = `
+    .then((res) => res.json())
+    .then((data) => {
+      // Get detail result container and clear text content
+      const detailResult = document.getElementById("detail-result");
+      detailResult.textContent = "";
+      // Get Result container div and clear text content
+      const containerDiv = document.getElementById("container-div");
+      containerDiv.textContent = "";
+      // Error Checking
+      if (searchText == 0) {
+        return;
+      } else if (!data.data.length) {
+        toggleElementDisplay("error-message", "block");
+      }
+      // Else
+      else {
+        // hide previous error message (if it's already showing)
+        toggleElementDisplay("error-message", "none");
+        // Slice fetched object upto 20 properties
+        const phones = Object.keys(data.data)
+          .slice(0, 20)
+          .map((key) => ({ 0: data.data[key] }));
+        // Get Each phones indivisually using for loop
+        for (const phone of phones) {
+          // Create and append cards using bootstrap styles
+          const div = document.createElement("div");
+          div.innerHTML = `
         <div class="card p-3">
           <img src=${phone[0].image} class="card-img-top" />
           <div class="card-body">
@@ -31,27 +48,34 @@ const displayPhones = () => {
           </div>
           <button id=${phone[0].slug} onclick="displayDetails(this.id)" class="btn btn-dark">Details</button>
           </div>
-          `
-          div.classList.add('col-md-4')
-          containerDiv.appendChild(div)
-          document.getElementById('search-field').value = ''
+          `;
+          div.classList.add("col-md-4");
+          containerDiv.appendChild(div);
+          // Clear Search Field Value
+          document.getElementById("search-field").value = "";
+        }
       }
-    }
-  })
-  }
+    });
+};
+
+// Details Button Click Handler
 const displayDetails = (clickedId) => {
-  const detailResult = document.getElementById('detail-result');
-  detailResult.textContent = ''
-  const url = `https://openapi.programming-hero.com/api/phone/${clickedId}`
-  // console.log(url)
+  // Get detail result container and clear text content
+  const detailResult = document.getElementById("detail-result");
+  detailResult.textContent = "";
+  // Get Dynamic API url
+  const url = `https://openapi.programming-hero.com/api/phone/${clickedId}`;
+  // Fetch Details API
   fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    const release = data.data.releaseDate ? data.data.releaseDate: "No Release Date Found"
-    const div = document.createElement('div');
-    
-  div.innerHTML = `
+    .then((res) => res.json())
+    .then((data) => {
+      const release = data.data.releaseDate
+        ? data.data.releaseDate
+        : "No Release Date Found";
+      // Create and append div using bootstrap styles
+      const div = document.createElement("div");
+
+      div.innerHTML = `
     <div class="col-md-6">
       <img class="d-block w-75 mx-auto" src=${data.data.image}>
     </div>
@@ -64,7 +88,7 @@ const displayDetails = (clickedId) => {
         <li>Display Size: ${data.data.mainFeatures.displaySize}</li>
         <li>Chipset: ${data.data.mainFeatures.chipSet}</li>
         <li>Memory: ${data.data.mainFeatures.memory}</li>
-        <li>Sensors: ${data.data.mainFeatures.sensors}</li>
+        <li>Sensors: ${data.data.mainFeatures.sensors.join(", ")}</li>
       </ul>
       <h3>Other Features</h3>
       <ul>
@@ -76,24 +100,8 @@ const displayDetails = (clickedId) => {
         <li>USB: ${data.data.others.USB}</li>
       </ul>
     </div>
-  `
-  div.classList.add('row',)
-detailResult.appendChild(div)
-})  
-}
-
-
-
-
-
-
-
-/* data: {mainFeatures: {storage: "128GB/256GB storage, microSDXC",…}, slug: "samsung_galaxy_tab_s8+-11342",…}
-brand: "Samsung"
-image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-s8-plus.jpg"
-mainFeatures: {storage: "128GB/256GB storage, microSDXC",…}
-name: "Galaxy Tab S8+"
-others: {WLAN: "Wi-Fi 802.11 a/b/g/n/ac/6e, dual-band, Wi-Fi Direct, hotspot", Bluetooth: "5.2, A2DP, LE",…}
-releaseDate: "Exp. release 2022, February"
-slug: "samsung_galaxy_tab_s8+-11342"
-status: true */
+  `;
+      div.classList.add("row");
+      detailResult.appendChild(div);
+    });
+};
